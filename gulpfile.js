d0d11231252bfad8444f0d7853a795ee2ecad19e
hehui <304574254@qq.com>
2015-10-23 02:53:10 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -21,100 +21,144 @@ var gulp = require('gulp'),
 
 
 var pkg = require('./package.json');
-var config = {
+var cf = {
     src: {
-        scss: "./scss/**/*.scss",
-        js: "./js/**/*.js",
-        img: "./img/**/*",
-        sprite: './img/myicon/*.png',
-        css: "./dist/css/**/*.css"
+        file: {
+            scss: 'scss/**/*.scss',
+            commCss: 'css/*.css',
+            js: 'js/**/*.js',
+            img: ['!img/myicon/*.*', 'img/**/*'],
+            sprite: 'img/myicon/*.png',
+            font: 'font/*'
+        },
+        dir: {
+            scss: 'scss/',
+            css: 'css/',
+            js: 'js/',
+            img: 'img/',
+            sprite: 'img/myicon/',
+            font: 'font/'
+        }
     },
     dist: {
-        js: './dist/js',
-        css: './dist/css',
-        fonts: './dist/fonts',
-        img: './dist/img',
-        maps: 'maps'
+        file: {
+            css: 'dist/css/**/*.css'
+        },
+        dir: {
+            js: 'dist/js',
+            css: 'dist/css',
+            img: 'dist/img',
+            font: 'dist/font'
+        }
     },
     autoprefixerBrowsers: [
-        "Android 2.3",
-        "Android >= 4",
-        "Chrome >= 20",
-        "Firefox >= 24",
-        "Explorer >= 8",
-        "iOS >= 6",
-        "Opera >= 12",
-        "Safari >= 6"
-    ]
+        'Android 2.3',
+        'Android >= 4',
+        'Chrome >= 20',
+        'Firefox >= 24',
+        'Explorer >= 8',
+        'iOS >= 6',
+        'Opera >= 12',
+        'Safari >= 6'
+    ],
+    spritePrefix: '.myicon-'
 };
 
 
 // ===============================================
 // style
-// 编译 sass 及生成 sourcemaps
-gulp.task('style', function () {
-    return sass(config.src.scss, { sourcemap: true })
+// sass
+gulp.task('sass', function () {
+    return sass(cf.src.file.scss, { sourcemap: true })
         .on('error', sass.logError)
         .pipe(autoprefixer({
-            browsers: config.autoprefixerBrowsers,
+            browsers: cf.autoprefixerBrowsers,
             cascade: false
         }))
-        .pipe(gulp.dest(config.dist.css))
-
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest(config.dist.css))
+        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('map', {
+            includeContent: false,
+            sourceRoot: 'scss'
+        }))
+        .pipe(gulp.dest(cf.src.dir.css));
+});
+// optimize css  优化样式
+gulp.task('optimize', function () {
+    return gulp.src(cf.src.file.commCss)
+        .pipe(concat('all.js'))
 
         .pipe(minifycss({compatibility: 'ie8'}))
         .pipe(rename({
             suffix: '.min',
             extname: '.css'
         }))
-
-        .pipe(sourcemaps.write())
-        .pipe(sourcemaps.write("./map", {
-            includeContent: false,
-            sourceRoot: 'scss'
-        }))
-        .pipe(gulp.dest(config.dist.css));
+        .pipe(gulp.dest(cf.dist.dir.css));
 });
-
+// style
+gulp.task('style', function() {
+    runSequence('sass', 'optimize');
+});
 
 // ========================================================
 // 图片
 gulp.task('img', function () {
-    return gulp.src(config.src.img)
+    return gulp.src(cf.src.file.img)
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}]
         }))
-        .pipe(gulp.dest(config.dist.img));
+        .pipe(gulp.dest(cf.dist.dir.img));
 });
 
 
 // 生成雪碧图
 gulp.task('sprite', function () {
     // Generate our spritesheet
-    var spriteData = gulp.src(config.src.sprite).pipe(spritesmith({
+    var spriteData = gulp.src(cf.src.file.sprite).pipe(spritesmith({
         imgName: 'sprite.png',
-        cssName: 'sprite.css'
+        cssName: 'sprite.css',
+        cssOpts: {
+            cssSelector: function (sprite) {
+                return cf.spritePrefix + sprite.name;  // 自定义className前缀
+            }
+        }
     }));
 
     // Pipe image stream through image optimizer and onto disk
     var imgStream = spriteData.img
         .pipe(imagemin())
-        .pipe(gulp.dest(config.dist.img));
+        .pipe(gulp.dest(cf.dist.dir.img));
 
     // Pipe CSS stream through CSS optimizer and onto disk
     var cssStream = spriteData.css
         //.pipe(csso())  // 压缩css
-        .pipe(gulp.dest(config.dist.css));
+        .pipe(gulp.dest(cf.dist.dir.css));
 
     // Return a merged stream to handle both `end` events
     return merge(imgStream, cssStream);
 });
 
-
 // 监听
+gulp.task('watch', function() {
+
+    // 监听所有.scss
+    gulp.watch(config.src.scss, ['styles']);
+
+    // 监听所有.js文件
+    gulp.watch(config.src.js, ['scripts']);
+
+    // 监听所有图片文件
+    gulp.watch(config.src.img, ['images']);
+
+    // 建立即时重整伺服器
+    var server = livereload();
+
+    // 监听所有位在 dist/  目录下的文件，一旦有更动，便进行更新
+    gulp.watch(['dist/**']).on('change', function(file) {
+        server.changed(file.path);
+    });
+
+});
 
 // 清理
 gulp.task('clean', function() {
